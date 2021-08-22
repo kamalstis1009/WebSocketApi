297bca6357e46d29eedd8727a9350b3e061239a6
kamalstis1009 <kamalstis1009@gmail.com>
2021-08-22 16:50:10 +0600

    Initial commit

--- src/main/java/com/example/websocket/config/WebMvcConfig.java
+++ src/main/java/com/example/websocket/config/WebMvcConfig.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig {
+
+}
+
+//@Configuration
+/*public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/broadcast").setViewName("/broadcast");
         registry.addViewController("/group").setViewName("/group");
         registry.addViewController("/chat").setViewName("/chat");
     }
-}
+}*/
